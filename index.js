ba93f82ec35329123651afa6b2ade033d6117e4f
AndruhaMan <andreysut12@gmail.com>
2023-10-12 23:18:57 +0300

    Fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "https://andruhaman.github.io/web-basics-lab4-client/",
+    origin: "https://andruhaman.github.io",
   }
 });
 

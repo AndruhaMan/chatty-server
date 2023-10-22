9ced68c8e244b6de15bf88ef2b8330ca9502d37d
AndruhaMan <andreysut12@gmail.com>
2023-10-22 19:14:35 +0300

    Update chat

--- index.js
+++ index.js
@@ -2,30 +2,69 @@ const express = require('express');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
+let USERS = [];
+const MESSAGES = [];
 
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "https://web-basics-lab4-client.onrender.com",
+    origin: [
+      "http://localhost:3000",
+      "https://web-basics-lab4-client.onrender.com",
+    ],
   }
 });
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/messages', (req, res) => {
+  res.json(MESSAGES);
+});
+
 io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`);
+  console.log(`${socket.id} connected ${new Date().toLocaleTimeString()}`);
+
+  socket.on('enterName', (userName, callback) => {
+    if (!USERS.find(user => user.userName === userName)) {
+      USERS.push({
+        id: socket.id,
+        userName: userName
+      });
+
+      callback({
+        status: 'ok'
+      })
+    } else {
+      callback({
+        status: 'error'
+      })
+    }
+  });
 
   socket.on('message', (data) => {
-    io.emit('messageResponse', data);
+    const userName = USERS.find(user => user.id === socket.id)?.userName;
+
+    if (userName) {
+      const message = {
+        ...data,
+        name: userName,
+        id: Date.now(),
+      };
+
+      MESSAGES.push(message);
+      io.emit('messageResponse', message);
+    }
+
   });
 
   socket.on('disconnect', () => {
-    console.log(`${socket.id} disconnected`);
+    USERS = USERS.filter(user => user.id !== socket.id);
+    console.log(`${socket.id} disconnected ${new Date().toLocaleTimeString()}`);
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log('Server is running');
 });
